5938cea1fe46832cd8813ede9efa1d58c3739857
Santosh Singh <santosh007bca@gmail.com>
2024-01-13 18:00:58 +0530

    working on cart page

--- src/shop/CartPage.jsx
+++ src/shop/CartPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import PageHeader from '../components/PageHeader';
+import { Link } from 'react-router-dom';
+import  delImgUrl from "../assets/images/shop/del.png";
 
 const CartPage = () => {
     const [cartItems, setCartItems] = useState([]);
@@ -60,6 +62,98 @@ const CartPage = () => {
     return (
         <div>
             <PageHeader title={"Shop Cart"} curPage={"Cart Page"} />
+            <div className='shop-cart padding-tb'>
+                <div className='container'>
+                    <div className='section-wrapper'>
+                        {/* cart top */}
+                        <div className='cart-top'>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th className='cat-product'>Product</th>
+                                        <th className='cat-price'>Price</th>
+                                        <th className='cat-quantity'>Quantity</th>
+                                        <th className='cat-toprice'>Total</th>
+                                        <th className='cat-edit'>Edit</th>
+                                    </tr>
+                                </thead>
+                                {/* table body */}
+                                <tbody>
+                                    {
+                                        cartItems.map((item, index) => (
+                                            <tr key={index}>
+                                               <td className='product-item cat-product'>
+                                                    <div className='p-thumb'>
+                                                        <Link to="/shop"><img src={item.img} alt={item.name} /></Link>
+                                                    </div>
+                                                    <div className='p-content'>
+                                                        <Link to="/shop">{item.name}</Link>
+                                                    </div>
+                                               </td> 
+                                               <td className='cat-price'>{item.price}</td>
+                                               <td className='cat-quantity'>
+                                                    <div className='cart-plus-minus'>
+                                                        <div className='dec qtybutton' onClick={() => handleDecrease(item)}>-</div>
+                                                        <input type='text' className='cart-plus-minus-box' name='qtybutton' value={item.quantity} />
+                                                        <div className='inc qtybutton' onClick={() => handleIncrease(item)}>+</div>
+                                                    </div>
+                                               </td>
+                                               <td className='cat-toprice'>$ {calculateTotalPrice(item)}</td>
+                                               <td className='cat-edit'>
+                                                    <a href='#' onClick={() => handleRemoveItem(item)}>
+                                                        <img src={delImgUrl} alt='delt' />
+                                                    </a>
+                                               </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+
+                        {/* cart top ends */}
+                        {/* cart bottom */}
+                        <div className='cart-bottom'>
+                            {/* checkout box*/}
+                            <div className='cart-checkout-box'>
+                                <form className='coupon'>
+                                    <input type='text' className='cart-page-input-text' name='coupon' id='coupon' placeholder='Coupon code...!!' />
+                                    <input type='submit' value={"Apply Coupon"} />
+                                </form>
+
+                                <form className='cart-checkout'>
+                                    <input type='text' value="Update Cart" />
+                                    <div >checkoutPage</div>
+                                </form>
+                            </div>
+                            {/* checkout box end */}
+
+                            {/* shopping box */}
+                            <div className='shiping-box'>
+                                <div className='row'>
+                                    <div className='col-md-6 col-12'>
+                                        <div className='calculate-shiping'>
+                                            <h3>Salculate Shiping</h3>
+                                            <div className='outline-select'>
+                                                <select>
+                                                    <option value="uk">United kingdom (UK)</option>
+                                                    <option value="usa">United State (USA)</option>
+                                                    <option value="in">India (in)</option>
+                                                    <option value="jp">Japan (JP)</option>
+                                                </select>
+                                                <span className='select-icon'>
+                                                    <i className='icofont-rounded-down'></i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='col-md-6 col-12'>Right Side</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
